98e633bdb59a2abc66a0641467bacf1bbea07c8e
magmafellow <magmafellow@gmail.com>
2024-06-13 23:51:41 +0300

    01 folder created

--- 03_integers/main.rs
+++ 03_integers/main.rs
@@ -136,8 +136,14 @@ fn main() {
     assert!(24 % 5 == 4);
 
     // Short-circuiting boolean logic
-    ..;
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
 
-    //Bitwise operations
-    ..;
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 }
